f6881438929d16dd6856a802fb568f293a7e6ede
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-08-21 21:10:46 +1200

    chore: move startrobocopy.ps1 script to ID609_OE1 directory

---
+++ ID609_OE1/startrobocopy.ps1
@@ -0,0 +1,40 @@
+function Start-Robocopy {
+param(
+  [Parameter(Mandatory=$true,
+           Position=0,
+           ValueFromPipeline=$true,
+           ValueFromPipelineByPropertyName=$true,
+           HelpMessage="Path to source of robocopy.")]
+  [Alias("PSSource")]
+  [ValidateNotNullOrEmpty()]
+  [string[]]
+  $Source,
+  [Parameter(Mandatory=$true,
+           Position=1,
+           ValueFromPipeline=$true,
+           ValueFromPipelineByPropertyName=$true,
+           HelpMessage="Path to destination of robocopy.")]
+  [Alias("PSDestination")]
+  [ValidateNotNullOrEmpty()]
+  [string[]]
+  $Destination
+)
+
+Write-Host "Executing robocopy from $Source to $Destination with log file at: $PSCommandPath\log.txt"
+# Calling robocopy with & opens it in a child shell
+# The /Z flag continues interrupted copies from their previous progress
+# The /MIR flag implies /E and /PURGE flags
+# The /E flag copies subdirectories including empty ones
+# The /PURGE flag deletes files from the destination folder that no longer exist in the source folder
+# The /MON:n flag waits for n changes before running robocopy again
+# The /MOT:m flag waits for m minutes before running robocopy again
+# The /X flag reports all eXtra files, not just files relevant to the copy operation
+# The /FP flag reports the full path names of all files
+# The /UNILOG+:file flag logs operations in unicode format appending to the path given in "file"
+& robocopy $Source $Destination /Z /MIR /MON:1 /MOT:1 /X /FP /UNILOG+:log.txt
+}
+
+Start-Robocopy -Source Files -Destination Files_backup
+
+
+
