2c7002755ceb00c8635b16468110af22fc37a2aa
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-09-03 03:21:05 +1200

    feat(ParseLog.psm1): add ConvertFrom-RobocopyLog

--- ID609_OE1/src/ParseLog.psd1
+++ ID609_OE1/src/ParseLog.psd1
@@ -70,8 +70,8 @@ Description = 'Provides functionality for parsing Robocopy log files.'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
-    "Get-LogData"#,
-    #"Convert-LogDataToHTML"
+    "Get-LogData",
+    "ConvertFrom-RobocopyLog"
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- ID609_OE1/src/ParseLog.psm1
+++ ID609_OE1/src/ParseLog.psm1
@@ -60,6 +60,37 @@ function Get-LogData {
     return $data -join "`n"
 }
 
-    }
+function ConvertFrom-RobocopyLog {
+    param (
+        # Robocopy log data
+        [CmdletBinding()]
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [AllowEmptyString()]
+        [string]
+        $LogData
+    )
+
+    $data = $LogData -split "`n"
+
+    # Extract all the lines with new files and deleted files (extra files)
+    # Matching on a string[] will return a string[] where the match is true
+    $data = $data -match "(\*EXTRA)|(New File)"
+
+    # $data
+    # throw "StopPls"
+        
+    # Loop for each found line
+    $data | ForEach-Object {
+        $_ -match "(?<Op>EXTRA|New) (?<Type>File|Dir).*\t(?<Name>[^\t\n]*$)" | Out-Null
+        # "Line: $_"
+        # "Matches.Type: $($Matches.Type)"
+        # "Matches.Name: $($Matches.Name)"
+        if ($Matches.Op -contains "New") {
+            "CREATED " + $Matches.Type + " " + $Matches.Name 
+        }
+        elseif ($Matches.Op -contains "EXTRA") { 
+            "DELETED " + $Matches.Type + " " + $Matches.Name 
+        }
     }
 }
