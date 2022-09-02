fb35165c7a515b66f7e5104343d8ff78ab536b56
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-09-03 02:28:53 +1200

    fix(ParseLog.psm1): return log file from StartTime when given

--- ID609_OE1/src/ParseLog.psm1
+++ ID609_OE1/src/ParseLog.psm1
@@ -27,7 +27,37 @@ function Get-LogData {
         return $data
     }
     else {
-        Write-Host $data
-        Write-Host "Returning log data from $StartTime"
+        # Regular expression matching the lines where robocopy started
+        $regex = "^.*Started : .*$"
+
+        # Get all robocopy starting lines
+        $starttimes = $data -match $regex
+
+        # Iterate the matched end times in reverse to find the date closest to and also before the start time
+        for ($i = $starttimes.Count - 1; $i -ge 0 ; $i--) {
+            <# 
+                $date1 = [DateTime]"9999-00-00T00:00"
+                $date2 = [DateTime]"2000-00-00T00:00"
+                $date1.CompareTo($date2) will return 1 because $date1 is a later date than $date2
+                Possible results for [DateTime].CompareTo():
+                $date1 -lt $date2 = -1
+                $date1 -eq $date2 = 0
+                $date1 -gt $date2 = 1
+            #>
+            # This debug string is too handy to delete.
+            #"$($i.ToString().PadLeft(2,"0"))`: $([DateTime]($starttimes[$i] -split "^.{3}Ended : ")[1])"
+            <#  
+                If the result of this comparison is -1, I.E. the current end time is older than the StartTime,
+                then the previous endtime is the one we care about, since we want the last data just before StartTime.
+            #>
+            if (($StartTime).CompareTo(([DateTime]($starttimes[$i] -split "^.{2}Started : ")[1])) -eq -1) {
+                # Get the index of that line in the string array
+                $startindex = $data.IndexOf($starttimes[$i - 1])
+            }
+        }
+
+        # Return all the data from the matched regex line to the end of the array
+        return $data[$startindex..$data.Length]
+    }
     }
 }
