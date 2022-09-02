1de141b4fb00d24f6320da2fca18fd6ba72e8185
Steffen Geving <GEVIS1@student.op.ac.nz>
2022-09-02 21:31:49 +1200

    feat: add elevation check to Start-LoggingFiles
    elevation is required to write to the event log

--- ID609_OE1/Start-LoggingFiles.ps1
+++ ID609_OE1/Start-LoggingFiles.ps1
@@ -1,3 +1,13 @@
+<#
+    Elevation check. This script should run as administrator.
+    Source: https://superuser.com/a/756696
+#>
+if (!([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent() `
+    ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    throw "This script needs Adminstrator privileges."
+}
+
 <#  
     Make sure our path is set to the root of the script,
     so paths to modules and log files are correct.
